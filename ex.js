c41ac602419158e8dcf747a193e148d81225bf3f
RodrigoSMG <162715071+RodrigoSMG@users.noreply.github.com>
2024-08-02 11:18:23 -0300

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,42 @@
+let primeiroNome = "Rodrigo"
+let ultimoNome = "Gomes"
+
+let nomeCompleto = primeiroNome + " " + ultimoNome
+console.log(nomeCompleto)
+
+//ex 2
+let numero1 = 10
+let numero2 = 20
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let divisao = numero1 / numero2
+let multiplicacao = numero1 * numero2
+console.log(soma, subtracao, divisao, multiplicacao )
+
+let estachovendo
+if(estachovendo){
+    console.log("estachovendo")
+
+}
+else{
+    console.log("nãoestachovendo")
+}
+
+
+
+//nota
+let resposta = null 
+let nota =
+console.log(resposta, nota)
+
+let idUnico = Symbol()
+letproduto = {
+    [idUnico]:123,
+
+nome: "camiseta"
+}
+console.log(produto[idUnico],produto.nome)
+let numeroGrande = BigInt("1234567456369258147258369258147258369258147")
+let numero = 1234567456369258147258369258147258369258147
+console.log(numeroGrande, numero)
