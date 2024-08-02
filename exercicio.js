050e1115396ad9b3a0205546ed861a816486623d
RodrigoSMG <162715071+RodrigoSMG@users.noreply.github.com>
2024-08-02 12:02:04 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+let frase = "JavaScript é incrível"
+//metodo leght que conta  o mumero de caracteres
+let comprimento = frase.length
+console.log(comprimento)
+let palavra = frase.slice(13 ,21)
+console.log(palavra)
+
+let substituir = frase.replace("incrivel","fantastico")
+console.log(substituir)
